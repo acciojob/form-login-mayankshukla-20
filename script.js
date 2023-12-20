ae390ff476c53f10e09d35ec615f17f2a46eb70b
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2023-12-20 15:03:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function getFormvalue() {
     //Write your code here
 	const fname = form.fname.value;
 	const lname = form.lname.value;
-	alert(fname + " " + lname)
+	alert(fname + " " + lname);
 }
